d483571cfd92e1866ff2f5afbf5929ce4297a1a2
Isfandiyor0112-star <imamalievisfandier@gmail.com>
2025-11-28 17:06:13 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,37 +2,37 @@ import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
 
-
 const app = express();
-app.use(express.static("public"));
 app.use(cors({ origin: "*" }));
 
-// список каналов
 const CHANNELS = {
-  sporttvuz: "https://st.uzlive.ru/sportuztv/tracks-v1a1/mono.ts.m3u8",
-  futboltvuz: "https://st.uzlive.ru/futboltvuz/tracks-v1a1/mono.ts.m3u8",
-  setanta1: "https://st.uzlive.ru/setanta-1/tracks-v1a1/mono.ts.m3u8"
+  futboltvuz: "https://st.uzlive.ru/futboltvuz/index.m3u8",
+  sporttvuz: "https://st.uzlive.ru/sportuztv/index.m3u8",
+  setanta1: "https://st.uzlive.ru/setanta-1/index.m3u8"
 };
 
-// универсальный прокси
 app.get("/channel/:name", async (req, res) => {
   const url = CHANNELS[req.params.name];
-  if (!url) {
-    res.status(404).send("Канал не найден");
-    return;
-  }
+  if (!url) return res.status(404).send("Канал не найден");
 
   try {
     const response = await fetch(url, {
       headers: {
-        "User-Agent": "Mozilla/5.0",
-        "Referer": "https://st.uzlive.ru/"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+        "Referer": "https://futboll.tv/",
+        "Origin": "https://futboll.tv",
+        "Accept": "*/*",
+        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Accept-Encoding": "gzip, deflate, br, zstd"
+        // Если появятся куки или x-vsaas-session — добавь сюда
       }
     });
+
     if (!response.ok) {
       res.status(response.status).send("Ошибка доступа к каналу");
       return;
     }
+
     response.body.pipe(res);
   } catch (err) {
     res.status(500).send("Ошибка прокси: " + err.message);
@@ -40,3 +40,4 @@ app.get("/channel/:name", async (req, res) => {
 });
 
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+
